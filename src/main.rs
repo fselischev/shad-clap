f38bab411f73d4fdbe07423c90d40dd070bee01a
Fedor Selischev <yanerala@gmail.com>
2024-09-17 21:30:43 +0300

    colorized

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use colored::Colorize;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{
     fs,
@@ -8,13 +9,14 @@ use std::{
 
 /// Finder CLI app.
 ///
-/// Finds all occurences of pattern in file by given path in parallel.
+/// Finds all occurrences of pattern in file by given path in parallel.
 #[derive(Parser)]
 #[command(name = "Finder", version)]
-#[command(override_usage = "finder --pattern <PATTERN> --path <FILE>")]
 #[command(before_help = "It's better to use ripgrep :)")]
 #[command(after_help = "Author: @fselischev")]
 struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
     /// Pattern to be found.
     ///
     /// Arbitrary string value that will be searched in files.
@@ -25,28 +27,48 @@ struct Args {
     paths: Vec<PathBuf>,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// Colorizes pattern in lines for readability (performance overhead).
+    Colorize,
+}
+
+fn process_file(path: &PathBuf, pattern: &str, colorize: bool) {
+    if let Ok(file) = fs::File::open(path) {
+        BufReader::new(file)
+            .lines()
+            .enumerate()
+            .filter_map(|(num, line)| {
+                line.ok().and_then(|l| {
+                    if l.contains(pattern) {
+                        let formatted_line = if colorize {
+                            let colored_line = l.replace(pattern, &pattern.red().to_string());
+                            format!(
+                                "{}:{}: {}",
+                                path.display().to_string().magenta(),
+                                (num + 1).to_string().green(),
+                                colored_line
+                            )
+                        } else {
+                            format!("{}:{}: {}", path.display(), num + 1, l)
+                        };
+                        Some(formatted_line)
+                    } else {
+                        None
+                    }
+                })
+            })
+            .for_each(|line| println!("{}", line));
+    } else {
+        eprintln!("Error: Could not open file {:?}", path);
+    }
+}
+
 fn main() {
     let args = Args::parse();
+    let colorize = matches!(args.command, Some(Commands::Colorize));
 
     args.paths.par_iter().for_each(|path| {
-        if let Ok(file) = fs::File::open(path) {
-            BufReader::new(file)
-                .lines()
-                .enumerate()
-                .filter_map(|(num, line)| {
-                    line.ok().and_then(|l| {
-                        if l.contains(&args.pattern) {
-                            Some(format!("{}:{}: {}", path.display(), num + 1, l))
-                        } else {
-                            None
-                        }
-                    })
-                })
-                .for_each(|line| println!("{}", line));
-        } else {
-            eprintln!("Error: Could not open file {:?}", path);
-        }
-
-        println!()
+        process_file(path, &args.pattern, colorize);
     });
 }
